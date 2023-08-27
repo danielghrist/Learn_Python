30e8eb4c11343224d66495fb1084c3cae2bbb288
Danny Ghrist <danielghrist@gmail.com>
2023-08-27 17:06:10 -0500

    Learning some Pandas

--- 100_Days_Code/Day 25/Squirrel Data Analysis/main.py
+++ 100_Days_Code/Day 25/Squirrel Data Analysis/main.py
@@ -1,8 +1,28 @@
-import pandas
+import pandas as pd
 
-data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
-#print(data["Primary Fur Color"].value_counts())
+# data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+# print(data["Primary Fur Color"].value_counts())
 
-new_dataframe = pandas.DataFrame(data["Primary Fur Color"].value_counts())
-print(new_dataframe)
-new_dataframe.to_csv("Squirrel Color Counts.csv")
+# new_dataframe = pandas.DataFrame(data["Primary Fur Color"].value_counts())
+# print(new_dataframe)
+# new_dataframe.to_csv("Squirrel Color Counts.csv")
+
+data = pd.read_csv(
+    "./100_Days_Code/Day 25/Squirrel Data Analysis/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+print(type(data))
+
+# Find number of NaN, Null, or None entries in the Primary Fur Color column:
+# data = data["Primary Fur Color"].isnull().value_counts()
+# print(data)
+
+# Find number squirrel by color:
+# data = data["Primary Fur Color"].dropna(
+# ).value_counts().reset_index(name="Fur Color")
+data = data["Primary Fur Color"].value_counts(dropna=False).reset_index()
+data.columns = ["Fur Color", "Count"]
+# data = pd.value_counts(data["Primary Fur Color"], dropna=False)
+# data = pd.DataFrame(data)
+# data.set_axis(["Fur Color"], axis="columns", inplace=True) # Rename columns
+# data = data["Primary Fur Color"].isnull().value_counts()
+print(type(data))
+print(data)
--- 100_Days_Code/Day 25/US States Game/main.py
+++ 100_Days_Code/Day 25/US States Game/main.py
@@ -9,6 +9,7 @@ import pandas
 
 WIDTH = 725
 HEIGHT = 491
+FILE_PATH = "./100_Days_Code/Day 25/US States Game/"
 
 screen = turtle.Screen()
 screen.title("U.S. States Guessing Game")
@@ -17,10 +18,11 @@ screen_writer = turtle.Turtle()
 screen_writer.penup()
 screen_writer.hideturtle()
 
-image = "blank_states_img.gif"
+image = f"{FILE_PATH}blank_states_img.gif"
 screen.addshape(image)
-turtle.shape(image)
-state_data = pandas.read_csv("50_states.csv")
+screen.bgpic(image)
+# turtle.shape(image)
+state_data = pandas.read_csv(f"{FILE_PATH}50_states.csv")
 
 
 # Code to pull x and y coordinates from the list of states DataFrame
@@ -33,14 +35,15 @@ def get_state_coors(player_guess):
 # Write the state name the player entered onto the screen
 def write_state_to_screen(turtle, player_guess):
     turtle.goto(get_state_coors(player_guess))
-    turtle.write(arg=player_guess, align="center", font=("Arial", 10, "normal"))
+    turtle.write(arg=player_guess, align="center",
+                 font=("Arial", 10, "normal"))
 
 
 # Retun True if player's guess was in the DataFrame of states
-def check_player_guess(player_guess, df):
+def check_player_guess(player_guess, correct_guesses: list, df: pandas.DataFrame):
     states = df.state.to_list()
     for state in states:
-        if player_guess == state:
+        if player_guess == state and correct_guesses.count(state) == 0:
             return True
     return False
 
@@ -53,14 +56,18 @@ correct_answers = []
 
 
 while not is_game_over:
-    player_answer = screen.textinput(title=text_input_title, prompt="Guess the name of a state").title()
+    player_answer = screen.textinput(
+        title=text_input_title, prompt="Guess the name of a state").title()
 
     if player_answer == "Exit":
         states = state_data.state.to_list()
         # List comprehension does the same thing as the commented out for loop below
-        not_guessed_states = [state for state in states if state not in correct_answers]
-        turtle.write(f"You guessed {num_correct_answers} out of {total_states} states.\n", align="center", font=("Arial", 25, "bold"))
-        turtle.write(f"The states you missed have been saved", align="center", font=("Arial", 25, "bold"))
+        not_guessed_states = [
+            state for state in states if state not in correct_answers]
+        turtle.write(f"You guessed {num_correct_answers} out of {total_states} states.\n",
+                     align="center", font=("Arial", 25, "bold"))
+        turtle.write(f"The states you missed have been saved",
+                     align="center", font=("Arial", 25, "bold"))
         with open("player_save_data.txt", "w") as file:
             # for state in states:
             #     if state not in correct_answers:
@@ -72,7 +79,7 @@ while not is_game_over:
     # Check if player correctly guessed a state
     # print(check_player_guess(player_answer, state_data))
 
-    if check_player_guess(player_answer, state_data):
+    if check_player_guess(player_answer, correct_guesses=correct_answers, df=state_data):
         num_correct_answers += 1
         write_state_to_screen(screen_writer, player_answer)
         text_input_title = f"{num_correct_answers}/{total_states} Correct"
@@ -80,7 +87,8 @@ while not is_game_over:
 
     if num_correct_answers == 50:
         turtle.home()
-        turtle.write("You guessed all the states.  You win!", align="center", font=("Arial", 25, "bold"))
+        turtle.write("You guessed all the states.  You win!",
+                     align="center", font=("Arial", 25, "bold"))
         is_game_over = True
 
 
