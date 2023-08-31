46910e97ebb134eb32ddb109f57eb7c3835c2fba
Danny Ghrist <danielghrist@gmail.com>
2023-08-31 18:53:56 -0500

    Day35 Rain Alert Project (won't work anymore)

--- 100_Days_Code/Day35/Rain Alert/main.py
+++ 100_Days_Code/Day35/Rain Alert/main.py
@@ -6,8 +6,9 @@ import os
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall"
 MY_LAT = 29.615860
 MY_LON = -98.599716
-API_KEY = "257e8124c5c40b3494e3de679fa1d0e1"
-# API_KEY = os.environ.get("OWM_API_KEY")
+API_KEY = os.environ.get('OWM_API_KEY')
+print(os.getenv('OWM_API_KEY'))
+print(API_KEY)
 
 parameters = {
     "lat": MY_LAT,
@@ -16,32 +17,32 @@ parameters = {
     "exclude": "current,minutely,daily,alerts"
 }
 
-response = requests.get(OWM_ENDPOINT, params=parameters)
-response.raise_for_status()
+# response = requests.get(OWM_ENDPOINT, params=parameters)
+# response.raise_for_status()
 # Prints the HTTP status code, 200 for success
 # print(response)
-weather_data = response.json()
+# weather_data = response.json()
 
 # Using Slicing to obtain only the next 12 hours of data
-next_12_hours = weather_data["hourly"][:12]
+# next_12_hours = weather_data["hourly"][:12]
 
 # Using Dictionary Comprehension to create a new dictionary which contains the hour: idcode as key value pairs
-weather_condition = {n: next_12_hours[n]["weather"][0]["id"] for n in range(len(next_12_hours))
-                     if next_12_hours[n]["weather"][0]["id"] < 700}
+# weather_condition = {n: next_12_hours[n]["weather"][0]["id"] for n in range(len(next_12_hours))
+#  if next_12_hours[n]["weather"][0]["id"] < 700}
 
 # print(weather_condition)
 
 # Determine what hours it will rain
-current_hour = datetime.now().hour
+# current_hour = datetime.now().hour
 
-if len(weather_condition) > 0:
-    for hour, idcode in weather_condition.items():
-        display_hour = hour + current_hour
-        if display_hour >= 24:
-            display_hour = 0 + hour
-        print(f"{display_hour:02}:00 has a chance of rain that hour")
-else:
-    print("Looks like its all clear for the next 12 hours.")
+# if len(weather_condition) > 0:
+#     for hour, idcode in weather_condition.items():
+#         display_hour = hour + current_hour
+#         if display_hour >= 24:
+#             display_hour = 0 + hour
+#         print(f"{display_hour:02}:00 has a chance of rain that hour")
+# else:
+#     print("Looks like its all clear for the next 12 hours.")
 
 # ***TEST STUFF I WAS PLAYING AROUND WITH***
 # print(json.dumps(next_12_hours, indent=4))
